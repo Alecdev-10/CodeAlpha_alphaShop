961d6e7c80608fa44257ad9377efcd9467f9f791
Alecdev-10 <kounouewaa@gmail.com>
2026-07-06 01:06:21 +0100

    Add migrations package initialization

--- marketPlace/apps.py
+++ marketPlace/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class MarketplaceConfig(AppConfig):
-    name = 'marketPlace'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "marketPlace"
